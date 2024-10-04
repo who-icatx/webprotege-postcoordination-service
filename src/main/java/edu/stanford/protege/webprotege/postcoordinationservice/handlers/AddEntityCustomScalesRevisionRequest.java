8ee2278ddcebbad52ce69c707f63f3cff7c3a55f
Gheorghe Soimu <soimugeo@gmail.com>
2024-10-04 18:34:08 +0300

    small changes to customScaleRevisionRequest

--- src/main/java/edu/stanford/protege/webprotege/postcoordinationservice/handlers/AddEntityCustomScalesRevisionRequest.java
+++ src/main/java/edu/stanford/protege/webprotege/postcoordinationservice/handlers/AddEntityCustomScalesRevisionRequest.java
@@ -1,10 +1,8 @@
 package edu.stanford.protege.webprotege.postcoordinationservice.handlers;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import edu.stanford.protege.webprotege.common.ProjectId;
-import edu.stanford.protege.webprotege.common.Request;
+import com.fasterxml.jackson.annotation.*;
+import edu.stanford.protege.webprotege.common.*;
 import edu.stanford.protege.webprotege.postcoordinationservice.model.WhoficCustomScalesValues;
 
 import static edu.stanford.protege.webprotege.postcoordinationservice.handlers.AddEntityCustomScalesRevisionRequest.CHANNEL;
@@ -17,8 +15,18 @@ public record AddEntityCustomScalesRevisionRequest(@JsonProperty("projectId")
 
     public final static String CHANNEL = "webprotege.postcoordination.AddEntityCustomScalesRevision";
 
+    @JsonCreator
+    public static AddEntityCustomScalesRevisionRequest create(@JsonProperty("projectId")
+                                                              ProjectId projectId,
+                                                              @JsonProperty("entityCustomScaleValues")
+                                                              WhoficCustomScalesValues entityCustomScaleValues) {
+        return new AddEntityCustomScalesRevisionRequest(projectId, entityCustomScaleValues);
+    }
+
     @Override
     public String getChannel() {
         return CHANNEL;
     }
+
+
 }
