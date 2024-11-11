a8a1ede9babcf129ad385a3a2abad0c97e4fdd6c
soimugeo <soimugeo@gmail.com>
2024-11-11 11:52:07 +0200

    Added ChangeType to ProjectChangeForEntity

--- src/main/java/edu/stanford/protege/webprotege/postcoordinationservice/uiHistoryConcern/changes/ProjectChangeForEntity.java
+++ src/main/java/edu/stanford/protege/webprotege/postcoordinationservice/uiHistoryConcern/changes/ProjectChangeForEntity.java
@@ -1,12 +1,28 @@
 package edu.stanford.protege.webprotege.postcoordinationservice.uiHistoryConcern.changes;
 
 import edu.stanford.protege.webprotege.change.ProjectChange;
+import org.jetbrains.annotations.NotNull;
 
 public record ProjectChangeForEntity(String whoficEntityIri,
-                                     ProjectChange projectChange) {
+                                     ChangeType changeType,
+                                     ProjectChange projectChange) implements Comparable<ProjectChangeForEntity> {
 
     public static ProjectChangeForEntity create(String whoficEntityIri,
+                                                ChangeType changeType,
                                                 ProjectChange projectChange) {
-        return new ProjectChangeForEntity(whoficEntityIri, projectChange);
+        return new ProjectChangeForEntity(whoficEntityIri, changeType, projectChange);
+    }
+
+    //All linearization/postcoordination changes are updates made on the whoficEntityIri.
+    // From this microservice we don't create or delete entities. that is the responsibility of the backend-service.
+    public static ProjectChangeForEntity create(String whoficEntityIri,
+                                                ProjectChange projectChange) {
+        return new ProjectChangeForEntity(whoficEntityIri, ChangeType.UPDATE_ENTITY, projectChange);
+    }
+
+    @Override
+    public int compareTo(@NotNull ProjectChangeForEntity other) {
+        return Long.compare(this.projectChange.getTimestamp(), other.projectChange.getTimestamp());
+
     }
 }
