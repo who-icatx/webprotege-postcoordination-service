e3b9b4b7f4137c0bf4a2adc82d2e8fb80edd96d6
alexsilaghi <silaghialexmihail@gmail.com>
2025-06-09 16:37:57 +0300

    added entity Type logs.

--- src/main/java/edu/stanford/protege/webprotege/postcoordinationservice/handlers/GetEntityPostCoordinationCommandHandler.java
+++ src/main/java/edu/stanford/protege/webprotege/postcoordinationservice/handlers/GetEntityPostCoordinationCommandHandler.java
@@ -48,7 +48,7 @@ public class GetEntityPostCoordinationCommandHandler implements CommandHandler<G
         try {
             List<String> entityTypes = entityTypeExecutor.execute(new GetIcatxEntityTypeRequest(IRI.create(request.entityIRI()), request.projectId()), executionContext)
                     .get(5, TimeUnit.SECONDS).icatxEntityTypes();
-
+            LOGGER.info("Fetched entity type for {} : {} ", request.entityIRI(), entityTypes);
             GetEntityPostCoordinationResponse processedSpec = postCoordService.fetchHistory(request.entityIRI(), request.projectId(),  entityTypes);
 
             return Mono.just(processedSpec);
